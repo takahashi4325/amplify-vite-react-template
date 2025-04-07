e9c5a355b59197820e450d98682c444607f8bb0b
takeru.takahashi@asia-quest.jp <takeru.takahashi@asia-quest.jp>
2025-04-07 11:11:23 +0900

    auth 追加

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
