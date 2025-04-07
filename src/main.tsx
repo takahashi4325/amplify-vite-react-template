fb6b1598b68568b9fb9d6918108cb15d61f22f4d
takeru.takahashi@asia-quest.jp <takeru.takahashi@asia-quest.jp>
2025-04-07 14:09:12 +0900

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </React.StrictMode>
 );
